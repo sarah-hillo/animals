fda208b943b32e7da1cc219e74ee4a8efd20cbc3
sarah-hillo <alhillosarah@gmail.com>
2021-07-25 21:12:39 +0300

    changes

---
+++ horned-beasts/src/components/footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class Footer extends React.Component {
+
+    render() {
+        return (
+            <footer>
+                <p>The author is sarah</p>
+            </footer>
+        )
+    }
+}
+
+export default Footer;
