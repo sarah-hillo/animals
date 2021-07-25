79521374bb9e0acae377dfc31c54edf9f1fce733
sarah-hillo <alhillosarah@gmail.com>
2021-07-25 21:17:05 +0300

    new changes

--- horned-beasts/src/App.js
+++ horned-beasts/src/App.js
@@ -2,21 +2,22 @@
 // first we are import the react library
 import React from 'react';
 import './App.css';
-// we are going to import the class based component file 
 import Main from './components/main';
-// we created our first Class based Component
-// we inherited properties and behaviors from the React Component Class
+import Header from './components/header';
+import Footer from './components/footer';
+
 class App extends React.Component {
 
   render() {
     return (
       <div>
         {/* We are creating a new instances of the Main class component */}
+        <Header />
         <Main />
+        <Footer />
       </div>
     )
   }
 }
 
-// we exported our App component to be visible for other components to use
 export default App;
