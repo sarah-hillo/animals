fbe83b6d61f8c71f1c8edf96fae03215855dff03
sarah-hillo <alhillosarah@gmail.com>
2021-07-28 01:09:15 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -4,14 +4,53 @@ import "./App.css";
 import Main from "./components/main";
 import Header from "./components/header";
 import Footer from "./components/footer";
+import data from "./components/assets/data.json";
+import SelectedBeast from "./components/SelectedBeast";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "tit",
+      image_url: "imgu",
+      description: "des",
+      show: false,
+    };
+  }
+
+  modalData = (title, image_url, description) => {
+    this.setState({
+      title: title,
+      url: image_url,
+      description: description,
+    });
+  };
+
+  handleShow = () => {
+    this.setState({
+      show: true,
+    });
+  };
+
+  handleHide = () => {
+    this.setState({
+      show: false,
+    });
+  };
+
   render() {
     return (
       <div>
         {/* We are creating a new instances of the Main class component */}
         <Header />
-        <Main />
+        <SelectedBeast
+          show={this.state.show}
+          hidden={this.handleHide}
+          title={this.state.title}
+          image_url={this.state.image_url}
+          description={this.state.description}
+        />
+        <Main dataList={data} modalData={this.modalData} />
         <Footer />
       </div>
     );
---
+++ src/components/SelectedBeast.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+
+class SelectedBeast extends React.Component {
+  exitModal = () => {
+    this.props.hidden(false);
+  };
+  render() {
+    return (
+      <div>
+        <Modal show={this.props.showModal} onHide={this.exitModal}>
+          <Modal.Header closeButton>
+            <Modal.Title>{this.props.title}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <img src={this.props.image_url} alt={this.props.title} />
+            <p>{this.props.description}</p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.exitModal}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export default SelectedBeast;
