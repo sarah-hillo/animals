03f0b2bbfc6e383fa1ae4a19e7425a818a2f83f1
sarah-hillo <alhillosarah@gmail.com>
2021-07-29 22:38:40 +0300

    change

--- src/components/hornedBeast.js
+++ src/components/hornedBeast.js
@@ -29,8 +29,7 @@ class HornedBeasts extends React.Component {
   render() {
     console.log(this.props);
     return (
-      <div>
-        
+      <div>      
         <section>
           {/* <h1>{this.props.title} Title</h1>
         <p>{this.props.description} </p>
